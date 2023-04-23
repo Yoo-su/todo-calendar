1bc218b6e1384175d18843b7a0b6cc0fcb66647b
yoosu <rhan0871@naver.com>
2023-04-23 23:47:55 +0900

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import App from "./src/App";
+
+const app = document.querySelector('#app');
+new App(app);
---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import "./global.css"
+
+export default function App(app) {
+    const render = () => {
+
+    }
+
+    render();
+}
