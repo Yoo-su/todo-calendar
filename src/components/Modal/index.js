0b0344a9e39d319d930675de80455025ef441555
yoosu <rhan0871@naver.com>
2023-04-24 00:05:13 +0900

    feat: add TodoInput & Modal component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,77 @@
+import TodoInput from "../TodoInput";
+import TodoList from "../TodoList";
+
+export default function Modal(parent, renewCalendar) {
+    this.state = {
+        dateInfo: {
+            year: null,
+            month: null,
+            day: null,
+            dayName: null,
+        },
+
+        todos: []
+    }
+
+    const container = document.createElement('div');
+    const wrapper = document.createElement('div');
+    const header = document.createElement('div');
+    const content = document.createElement('div');
+    const todoList = new TodoList(content);
+    const todoInput = new TodoInput(content, todoList.addTodo);
+
+    this.setState = (nextState) => {
+        this.state = { ...this.state, ...nextState };
+        todoList.setState({ dateInfo: this.state.dateInfo });
+        render();
+    }
+
+    /* modal close */
+    const handleCloseBtnClick = () => {
+        container.style.display = "none";
+        todoInput.clearInput();
+        renewCalendar();
+    }
+
+    /* modal open */
+    const handleModalOpen = (data) => {
+        this.setState(data);
+        container.style.display = "flex"
+    }
+
+    /* 초기화 */
+    const init = () => {
+        container.className = "modal-container";
+        wrapper.className = "modal-wrapper";
+        header.className = "modal-header";
+        content.className = "modal-content";
+
+        header.onclick = (event) => {
+            if (event.target.tagName === 'IMG') handleCloseBtnClick();
+        }
+
+        container.appendChild(wrapper);
+        wrapper.appendChild(header);
+        wrapper.appendChild(content);
+
+        parent.appendChild(container);
+    }
+
+    /* render */
+    const render = () => {
+        const { year, month, day, dayName } = this.state.dateInfo;
+        header.innerHTML = ` 
+            <b>${year}.${month}.${day} ${dayName}</b>
+            <button class="modal-close-btn"><img src="/src/assets/images/close.png" alt="modal-close-btn" /></button>
+        `;
+
+        todoList.render(this.state.todos);
+    }
+
+    init();
+
+    return {
+        handleModalOpen,
+        handleCloseBtnClick
+    }
+}
